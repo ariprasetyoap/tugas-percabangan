8541354b55bb1c132b8e69de8af8d772e96df4ef
patricktelnoni <patricktelnoni@users.noreply.github.com>
2024-10-08 15:14:18 +0700

    Add files via upload

---
+++ TugasPercabangan.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TugasPercabangan{
+    public static void main(String[] args){
+        int lamaKerja, jumlahJamKerja;
+        double totalGaji;
+        
+
+        Scanner scanner = new Scanner(System.in);
+        lamaKerja       = scanner.nextInt();
+        jumlahJamKerja  = scanner.nextInt();
+
+        /* 
+            Tuliskan proses perhitungan gaji di sini
+        */
+        
+        System.out.println(totalGaji);
+    }
+}
